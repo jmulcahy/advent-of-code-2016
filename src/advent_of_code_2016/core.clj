40bc659b79e1234295cc9f6981b05a5b9dfd20fa
John Mulcahy-Stanislawczyk <6724499+jmulcahy@users.noreply.github.com>
2017-01-03 21:23:19 -0600

    added day12 part a, labeled as day2a for now

--- src/advent_of_code_2016/core.clj
+++ src/advent_of_code_2016/core.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2016.core
   (:gen-class)
-  (:require [advent-of-code-2016.day1 :as day1]))
+  (:require [advent-of-code-2016.day1 :as day1])
+  (:require [advent-of-code-2016.day12 :as day12]))
 
 (defn do-day1
   [part]
@@ -10,10 +11,23 @@
       (= part \b) (day1/part2 input)
       :else "Invalid Part")))
 
+(defn do-day12
+  [part]
+  (cond
+    (= part \a) (let [instruction-map {"cpy" day12/copy
+                                       "inc" day12/increment
+                                       "dec" day12/decrement
+                                       "jnz" day12/jump}
+                      registers {"a" 0 "b" 0 "c" 0 "d" 0}
+                      input (slurp (clojure.java.io/resource "day12a.txt"))]
+                  (day12/execute instruction-map registers input))
+    :else "Invalid Part"))
+
 (defn pick-day
   [day part]
   (cond
     (= day \1) (do-day1 part)
+    (= day \2) (do-day12 part)
     :else "Invalid Day"))
 
 (defn -main
